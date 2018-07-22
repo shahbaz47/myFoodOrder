aa3e61c686b83be918bf557282493a61ae0806a5
Shahbaz <shbz047@gmail.com>
2018-07-22 05:19:36 -0400

    added UserModels, Items and Restaurants Datamodels

---
+++ myFoodOrder/Items.cs
@@ -0,0 +1,10 @@
+﻿using Realms;
+
+public class Items : RealmObject
+{
+    public string iName { get; set; }
+    public string iDesc { get; set; }
+    public string iImg { get; set; }
+    public string rName { get; set; }
+    public double price { get; set; }
+}
---
+++ myFoodOrder/Restaurants.cs
@@ -0,0 +1,9 @@
+﻿using Realms;
+
+public class Restaurants : RealmObject
+{
+    public string rName { get; set; }
+    public string rAddr { get; set; }
+    public string rImg { get; set; }
+    public string rType { get; set; }
+}
